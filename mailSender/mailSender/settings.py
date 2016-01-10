cf38eeb20c68bb94042a87cf43b747f87ff47d45
tony158 <tangxinzhi158@hotmail.com>
2016-01-10 14:36:56 +0800

    correct URL path
    Change-Id: I25fb4bda8baf7ce395b29e807aaf95bd43d24e3a

--- mailSender/mailSender/settings.py
+++ mailSender/mailSender/settings.py
@@ -125,7 +125,7 @@ ROOT_URLCONF = 'mailSender.urls'
 WSGI_APPLICATION = 'mailSender.wsgi.application'
 
 TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, 'templates/'),
+    os.path.join(PROJECT_PATH, 'templates/'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
